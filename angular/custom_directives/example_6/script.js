1bf5edd7ffcdc114f2558e6216e21fe9f0e0591c
MIHIR PATEL <mihirptl89@gmail.com>
2015-08-05 07:54:13 -0600

    decorative directives

--- angular/custom_directives/example_6/script.js
+++ angular/custom_directives/example_6/script.js
@@ -1,6 +1,12 @@
 angular.module("app", []);
 
 angular.module("app").controller("mainCtrl", function($scope) {
+	$scope.messages = [];
+
+	$scope.handlePause = function(e) {
+		console.log(e);
+		$scope.messages.push({text: "paused"});
+	}
 	
 });
 
@@ -19,6 +25,19 @@ angular.module("app").directive("spacebarSupport", function() {
 				}
 			})
 		}
-
 	}
-})
+});
+
+angular.module("app").directive("eventPause", function($parse) {
+	return {
+		restirct: "A",
+		link: function(scope, el, attrs) {
+			var fn = $parse(attrs["eventPause"]);
+			el.on("pause", function(event) {
+				scope.$apply(function() {
+					fn(scope, {evt: event})
+				})
+			})
+		}
+	}
+});
